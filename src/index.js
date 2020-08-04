65e3084bd75d69ab5d60b66a708fef20a31c66be
Zam Hassan <zameerulhassan@gmail.com>
2020-08-04 11:43:31 -0600

    adds Context, Cart, and logic

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from "./App";
 //bringing in ProductProvider
 //and wrap whole App component with it
 import ProductProvider from './context/products'
+import {CartProvider} from './context/cart'
 ReactDOM.render(
     <ProductProvider>
+    <CartProvider>
     <App />
+    </CartProvider>
     </ProductProvider>,
      document.getElementById("root"));
  
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,5 +1,37 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import { ProductContext } from "../context/products";
+//import {CartContext} from '../context/cart'
+import { useHistory } from "react-router-dom";
+import Loading from "../components/Loading";
 export default function ProductDetails() {
-  return <h1>hello from product details page</h1>;
+  const { id } = useParams();
+  const history = useHistory();
+  const { products } = React.useContext(ProductContext);
+  const product = products.find((item) => item.id === parseInt(id));
+  if (products.length === 0) {
+    return <Loading />;
+  } else {
+    const {
+      image: { url },
+      price,
+      title,
+      description,
+    } = product;
+    return (
+      <section className="single-product">
+        <img src={url} alt={title} className="single-product-img" />
+        <article>
+        <h1>{title}</h1>
+        <h2>${price}</h2>
+        <p>{description}</p>
+        <button className="btn btn-primary btn-block" onClick={()=>{
+          //add to cart
+          //useHistory to push
+          history.push('/cart')
+        }}>add to cart</button>
+        </article>
+      </section>
+    );
+  }
 }
